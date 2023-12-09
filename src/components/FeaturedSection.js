06a2520848f23da9520ec0c84b4ca3066e0d5cb9
Jigar Patel <jpxtel98@outlook.com>
2023-12-08 23:10:04 -0500

    remove unused file and change featuredSection; going forward we'll get the feaurred data from DB

--- src/components/FeaturedSection.js
+++ src/components/FeaturedSection.js
@@ -4,41 +4,20 @@ import PromptCard from "./PromptCard";
 const FeaturedSection = () => {
   const featuredPrompts = [
     {
-      id: 1,
       title: "Prompt One",
       description: "This is a featured prompt.",
       category: "category1",
+      tags: ["tag1", "tag2", "tag3"],
+      featured: true,
     },
     {
       id: 2,
       title: "Prompt Two",
       description: "This is a featured prompt.",
       category: "category2",
-    },
-    {
-      id: 3,
-      title: "Prompt Three",
-      description: "This is a featured prompt.",
-      category: "category3",
-    },
-    {
-      id: 4,
-      title: "Prompt Four",
-      description: "This is a featured prompt.",
-      category: "category4",
-    },
-    {
-      id: 5,
-      title: "Prompt Five",
-      description: "This is a featured prompt.",
-      category: "category5",
-    },
-    {
-      id: 6,
-      title: "Prompt Six",
-      description: "This is a featured prompt.",
-      category: "category6",
-    },
+      tags: ["tag1", "tag2", "tag3"],
+      featured: true,
+    }
   ];
 
   return (
